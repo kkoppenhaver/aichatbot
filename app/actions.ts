b74fe08ea7fe4d5bc8058094731ff275883993c8
Keanan Koppenhaver <keanan@alphaparticle.com>
2024-01-15 18:48:30 -0600

    Get back to working version

--- app/actions.ts
+++ app/actions.ts
@@ -1,3 +1,5 @@
+'use server'
+
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { kv } from '@vercel/kv'
@@ -40,19 +42,19 @@ export async function getChat(id: string, userId: string) {
 export async function removeChat({ id, path }: { id: string; path: string }) {
   // const session = await auth()
 
-  if (!session) {
-    return {
-      error: 'Unauthorized'
-    }
-  }
+  // if (!session) {
+  //   return {
+  //     error: 'Unauthorized'
+  //   }
+  // }
 
   const uid = await kv.hget<string>(`chat:${id}`, 'userId')
 
-  if (uid !== session?.user?.id) {
-    return {
-      error: 'Unauthorized'
-    }
-  }
+  // if (uid !== session?.user?.id) {
+  //   return {
+  //     error: 'Unauthorized'
+  //   }
+  // }
 
   await kv.del(`chat:${id}`)
   await kv.zrem(`user:chat:${session.user.id}`, `chat:${id}`)
@@ -64,11 +66,11 @@ export async function removeChat({ id, path }: { id: string; path: string }) {
 export async function clearChats() {
   // const session = await auth()
 
-  if (!session?.user?.id) {
-    return {
-      error: 'Unauthorized'
-    }
-  }
+  // if (!session?.user?.id) {
+  //   return {
+  //     error: 'Unauthorized'
+  //   }
+  // }
 
   const chats: string[] = await kv.zrange(`user:chat:${session.user.id}`, 0, -1)
   if (!chats.length) {
@@ -100,11 +102,11 @@ export async function getSharedChat(id: string) {
 export async function shareChat(id: string) {
   // const session = await auth()
 
-  if (!session?.user?.id) {
-    return {
-      error: 'Unauthorized'
-    }
-  }
+  // if (!session?.user?.id) {
+  //   return {
+  //     error: 'Unauthorized'
+  //   }
+  // }
 
   const chat = await kv.hgetall<Chat>(`chat:${id}`)
 
--- components/sidebar-desktop.tsx
+++ components/sidebar-desktop.tsx
@@ -5,12 +5,16 @@ import { ChatHistory } from '@/components/chat-history'
 import { useStytchUser, useStytch } from "@stytch/nextjs";
 
 export async function SidebarDesktop() {
-  const { user, isInitialized } = useStytchUser();
-  const stytch = useStytch();
+  // const { user, isInitialized } = useStytchUser();
+  // const stytch = useStytch();
 
-  if (!user.user_id) {
-    return null
-  }
+  // if (!user.user_id) {
+  //   return null
+  // }
+
+  const user = {
+    user_id: 1
+  };
 
   return (
     <Sidebar className="peer absolute inset-y-0 z-30 hidden -translate-x-full border-r bg-muted duration-300 ease-in-out data-[state=open]:translate-x-0 lg:flex lg:w-[250px] xl:w-[300px]">
